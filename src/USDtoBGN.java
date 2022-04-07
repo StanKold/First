6dbe7aa79ae592a8b1d902ab259288742aceb2be
StanKold <sb.koldeev@gmail.bg>
2022-04-07 14:03:48 +0300

    Initial commit

---
+++ src/USDtoBGN.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class USDtoBGN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double usd = Double.parseDouble(scanner.nextLine());
+        System.out.println(usd*1.79549);
+
+    }
+}
