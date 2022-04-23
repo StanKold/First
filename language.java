55687966356bce27860f565a96f8f63c28dda53b
StanKold <103124654+StanKold@users.noreply.github.com>
2022-04-23 13:37:02 +0300

    Create language.java

---
+++ language.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class language {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Argentina":
+            case "Spain":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
