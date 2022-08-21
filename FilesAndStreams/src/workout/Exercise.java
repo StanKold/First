582cdf9077f7cad8b7e9384a501d8e396e968758
StanKold <103124654+StanKold@users.noreply.github.com>
2022-08-21 21:44:07 +0300

    Create Exercise.java

---
+++ FilesAndStreams/src/workout/Exercise.java
@@ -0,0 +1,45 @@
+package workout;
+
+public class Exercise {
+    String name;
+    String muscle;
+    int burnedCalories;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+
+    public void setMuscle(String muscle) {
+        this.muscle = muscle;
+    }
+
+    public int getBurnedCalories() {
+        return burnedCalories;
+    }
+
+    public void setBurnedCalories(int burnedCalories) {
+        this.burnedCalories = burnedCalories;
+    }
+
+
+    public Exercise(String name, String muscle, int burnedCalories) {
+        this.name = name;
+        this.muscle = muscle;
+        this.burnedCalories = burnedCalories;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Exercise: %s, %S, %d",this.name,this.muscle,this.burnedCalories);
+
+    }
+}
