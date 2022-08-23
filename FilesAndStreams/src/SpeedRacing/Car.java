dd96e7c9de3c052b34823fc956d0ad056f987310
StanKold <103124654+StanKold@users.noreply.github.com>
2022-08-23 23:16:42 +0300

    Create Car.java

---
+++ FilesAndStreams/src/SpeedRacing/Car.java
@@ -0,0 +1,32 @@
+package SpeedRacing;
+
+public class Car {
+    private String model;
+    private double fuel;
+    private double costs;
+    private double distance;
+
+
+    public Car(String model, double fuel, double costs, double distance) {
+        this.model = model;
+        this.fuel = fuel;
+        this.costs = costs;
+        this.distance = distance;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
