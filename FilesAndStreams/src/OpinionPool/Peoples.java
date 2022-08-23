e18966abea6cc0f552b5bc67a536fe4a47cf9b0d
StanKold <103124654+StanKold@users.noreply.github.com>
2022-08-23 23:16:47 +0300

    Create Peoples.java

---
+++ FilesAndStreams/src/OpinionPool/Peoples.java
@@ -0,0 +1,21 @@
+package OpinionPool;
+
+public class Peoples {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Peoples(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
