38a2185fbf1516de1b5f87905ba136873767286a
StanKold <103124654+StanKold@users.noreply.github.com>
2022-08-29 20:26:26 +0300

    Create Car.java

---
+++ FilesAndStreams/src/carClass/Car.java
@@ -0,0 +1,30 @@
+package carClass;
+
+public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+
+    public String carInfo() {
+        return String.format("The car is: %s %s - %d HP." ,getBrand(),getModel(),getHorsePower());
+    }
+}
